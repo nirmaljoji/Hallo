1476bf0d70c67d20c918f9b5eb6ff9e42ed06ded
Rakshana <rakshana.jp@gmail.com>
2020-01-30 23:46:40 +0530

    addition: added dummyLogo to screen
    Signed-off-by: Rakshana <rakshana.jp@gmail.com>

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -44,113 +44,122 @@ class _SignInState extends State<SignIn> {
             padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
             child: Form(
               key: _formkey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black12,
-                        width: 2.0,
+              child: Container(
+
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 150.0),
+                      child: Image.asset(
+                        'images/hallo.png',
+                        scale: 5,
                       ),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: TextFormField(
-                        validator: (val) =>
-                        val.isEmpty
-                            ? 'Enter an email'
-                            : null,
-                        style: TextStyle(color: Colors.white),
-
-                        decoration: InputDecoration(
-                          hintText: "Email-id",
-                          hintStyle: TextStyle(color: Colors.grey[400]),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black12,
+                          width: 2.0,
                         ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: TextFormField(
+                          validator: (val) =>
+                          val.isEmpty
+                              ? 'Enter an email'
+                              : null,
+                          style: TextStyle(color: Colors.white),
+
+                          decoration: InputDecoration(
+                            hintText: "Email-id",
+                            hintStyle: TextStyle(color: Colors.grey[400]),
+                          ),
 
 
-                        onChanged: (val) {
-                          setState(() {
-                            email = val;
-                          });
-                        },
+                          onChanged: (val) {
+                            setState(() {
+                              email = val;
+                            });
+                          },
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black12,
-                        width: 2.0,
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black12,
+                          width: 2.0,
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: TextFormField(
+                          validator: (val) =>
+                          val.length < 6
+                              ? 'Enter a password 6+ chars long'
+                              : null,
+                          decoration: InputDecoration(
+                              hintText: "Password",
+                              hintStyle: TextStyle(color: Colors.grey[400])),
+                          style: TextStyle(color: Colors.white),
+                          onChanged: (val) {
+                            setState(() {
+                              password = val;
+                            });
+                          },
+                          obscureText: true,
+                        ),
                       ),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: TextFormField(
-                        validator: (val) =>
-                        val.length < 6
-                            ? 'Enter a password 6+ chars long'
-                            : null,
-                        decoration: InputDecoration(
-                            hintText: "Password",
-                            hintStyle: TextStyle(color: Colors.grey[400])),
-                        style: TextStyle(color: Colors.white),
-                        onChanged: (val) {
-                          setState(() {
-                            password = val;
-                          });
-                        },
-                        obscureText: true,
+                    SizedBox(
+                      height: 20,
+                    ),
+                    RaisedButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
                       ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  RaisedButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                    ),
-                    padding: EdgeInsets.symmetric(
-                        vertical: 15, horizontal: 60),
-                    color: Colors.grey[900],
-                    child: Text(
-                      "Sign in",
-                      style: TextStyle(
-                        color: Colors.amberAccent[400],
-                        fontSize: 20,
+                      padding: EdgeInsets.symmetric(
+                          vertical: 12, horizontal: 50),
+                      color: Colors.lightGreen,
+                      child: Text(
+                        "Sign in",
+                        style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 23,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
 
-                    onPressed: () async {
-                      print("Email:$email");
-                      print("password:$password");
+                      onPressed: () async {
+                        print("Email:$email");
+                        print("password:$password");
 
-                      if (_formkey.currentState.validate()) {
-                        dynamic result = await _auth
-                            .signInWithEmailAndPassword(email, password);
-                        if (result == null) {
-                          setState(() {
-                            error = 'Could Not Sign in with Credentials';
-                          });
+                        if (_formkey.currentState.validate()) {
+                          dynamic result = await _auth
+                              .signInWithEmailAndPassword(email, password);
+                          if (result == null) {
+                            setState(() {
+                              error = 'Could Not Sign in with Credentials';
+                            });
+                          }
                         }
-                      }
-                    },
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    error,
-                    style: TextStyle(color: Colors.red, fontSize: 14),
-                  )
-                ],
+                      },
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.red, fontSize: 14),
+                    )
+                  ],
+                ),
               ),
             )));
   }
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -27,7 +27,6 @@ class _HomeState extends State<Home> {
         actions: <Widget>[
           FlatButton.icon(
               onPressed: () async{
-
                 await _auth.signOut();
               },
               icon: Icon(Icons.person),
