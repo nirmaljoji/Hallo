904378b0275db7e5c933a9772974b7499ce90f20
Rakshana <rakshana.jp@gmail.com>
2020-02-20 19:24:50 +0530

    design is left
    Signed-off-by: Rakshana <rakshana.jp@gmail.com>

--- lib/components/chat_button.dart
+++ lib/components/chat_button.dart
@@ -12,21 +12,49 @@ class ChatButton extends StatelessWidget {
     print('chat button $friendName');
     return Material(
       elevation: 2.0,
+      color: Colors.grey,
       child: MaterialButton(
         onPressed: onPressed,
         minWidth: 500.0,
         height: 70.0,
         child: ListTile(
-          leading: imageURL!=null?Image.network(
-            imageURL,
-            fit: BoxFit.cover ,
-          ):SizedBox(width: 1.0),
-          title: Text(
-            friendName,
-            style: TextStyle(
-              color: Colors.grey.shade900,
+          contentPadding: EdgeInsets.all(4.0),
+          leading: imageURL != null ? CircleAvatar(
+            backgroundImage: AssetImage('assets/'),
+            radius: 30.0,
+            child: ClipOval(
+              child: new SizedBox(
+                  width: 180,
+                  height: 180,
+                  child: imageURL != null ? Image.network(
+                    imageURL,
+                    fit: BoxFit.cover,
+                  ) : Container(
+                    color: Colors.amber,
+                  )
+              ),
             ),
+          ) : CircleAvatar(
+            backgroundImage: AssetImage('images/user.png'),
+            radius: 30.0,
           ),
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                friendName,
+                style: TextStyle(
+                  color: Colors.grey.shade900,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+            ],
+          ),
+
         ),
       ),
     );
