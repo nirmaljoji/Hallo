b2cae134fec165bc5d481a587364969b428182e1
Sharon Joji <sharonjoji99@gmail.com>
2020-02-26 22:41:36 +0530

    Frogot password working :)

--- lib/screens/authenticate/forgot_pwd.dart
+++ lib/screens/authenticate/forgot_pwd.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:hallo/components/hallo_button.dart';
 import 'package:hallo/components/hallo_text_field.dart';
 import 'package:hallo/models/uid.dart';
 import 'package:hallo/services/database.dart';
+import 'package:hallo/shared/hallo_theme_data.dart';
 
 
 class ForgotPwd extends StatefulWidget {
@@ -20,46 +22,104 @@ class _ForgotPwdState extends State<ForgotPwd> {
 
   bool loading = false;
 
+  // user defined function
+  void _showDialog(bool state) {
+    // flutter defined function
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Email Verification"),
+          content: state==false ? new Text("Failed ! , please provide a valid email") : new Text(" Successull ! Check your email to reset password"),
+          actions: <Widget>[
+            // usually buttons at the bottom of the dialog
+            new FlatButton(
+              child: new Text("Close"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-
-
-    void _showToast(BuildContext context) {
-      final scaffold = Scaffold.of(context);
-      scaffold.showSnackBar(
-        SnackBar(
-          backgroundColor: Theme
-              .of(context)
-              .splashColor,
-          content: const Text('Error email not foung !'),
-          action: SnackBarAction(
-              label: 'RETRY', onPressed: scaffold.hideCurrentSnackBar),
-        ),
-      );
-    }
+    HalloThemeData data = new HalloThemeData();
 
 
     return Scaffold(
-      body: HalloTextField(
-        text: "",
-        hint: "Enter email",
-        isPassword: false,
-        onChangedText: (value) {
-          setState(() async {
-            email = value;
-            QuerySnapshot FutureValue =
-            await DatabaseService(uid: current_user_uid)
-                .checkIfMailExist(email);
-            if (FutureValue.documents.isNotEmpty) {
-              _FirebaseAuth.sendPasswordResetEmail(email: email);
+      backgroundColor: Theme
+          .of(context)
+          .backgroundColor,
+      appBar: AppBar(
+        backgroundColor: Theme
+            .of(context)
+            .accentColor,
+        elevation: 0.0,
+        title: Text(
+          data.hallo,
+          style: Theme
+              .of(context)
+              .textTheme
+              .title,
+        ),
+      ),
 
-              Navigator.pushNamed(context, '/login');
-            }
-            else {
-              _showToast(context);
-            }
-          });
-        },
+
+
+
+      body: Column(
+        children: <Widget>[
+          SizedBox(height: 20,),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
+            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 30.0),
+            child: HalloTextField(
+              text: "",
+              hint: "Enter email",
+              isPassword: false,
+              onChangedText: (value) {
+                setState(() async {
+                  email = value;
+
+                });
+              },
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          HalloButton(
+            color1: data.btnColor,
+            color2: data.cardColor,
+            text: "Send Verification",
+            onPressedBtn: () async {
+
+              QuerySnapshot FutureValue =
+                  await DatabaseService(uid: current_user_uid)
+                  .checkIfMailExist(email);
+              if (FutureValue.documents.isNotEmpty) {
+                _FirebaseAuth.sendPasswordResetEmail(email: email);
+
+
+                _showDialog(true);
+
+              }
+              else {
+                _showDialog(false);
+              }
+
+            },
+          )
+
+
+
+
+        ],
       ),
     );
 
