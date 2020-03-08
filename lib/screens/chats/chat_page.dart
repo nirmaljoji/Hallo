9f8415984533332a9ec283bab7eec89d6b8ed003
Rakshana <rakshana.jp@gmail.com>
2020-03-08 17:31:14 +0530

    ui for sending text done
    Signed-off-by: Rakshana <rakshana.jp@gmail.com>

--- lib/screens/chats/chat_page.dart
+++ lib/screens/chats/chat_page.dart
@@ -30,6 +30,7 @@ class ChatPage extends StatelessWidget {
               friendUID: friendUID,
             ),
             Container(
+              margin: EdgeInsets.fromLTRB(7.0, 0, 5.0, 5.0),
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
@@ -39,32 +40,73 @@ class ChatPage extends StatelessWidget {
                         onChanged: (val) {
                           msgText = val;
                         },
+                        autocorrect: true,
+                        decoration: InputDecoration(
+                          fillColor: Theme
+                              .of(context)
+                              .canvasColor,
+                          filled: true,
+                          contentPadding: EdgeInsets.all(12.0),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(60.0),
+                            borderSide: BorderSide(color: Theme
+                                .of(context)
+                                .canvasColor, width: 2.0),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(60.0),
+                            borderSide: BorderSide(color: Theme
+                                .of(context)
+                                .focusColor, width: 2.0),
+                          ),
+                          hintText: ' Type message',
+                          //hint
+                          hintStyle: Theme
+                              .of(context)
+                              .textTheme
+                              .body2,
+                        ),
                       )
                   ),
-                  FlatButton(
-                    child: Text('Send'),
+                  Container(
+                    width: 70.0,
+                    height: 70.0,
+                    padding: const EdgeInsets.all(8.0),
+                    child: RaisedButton(
+                      child: Icon(
+                        Icons.send,
+                        color: Theme
+                            .of(context)
+                            .canvasColor,
+                      ),
+                      onPressed: () {
+                        msgClear.clear();
 
-                    onPressed: () {
-                      msgClear.clear();
+                        _firestore.collection('messages').document(
+                            current_user_uid).collection(friendUID).add({
+                          'text': msgText,
+                          'time': DateTime.now(),
+                          'to': friendUID,
+                          'from': current_user_uid,
+                        });
 
-                      _firestore.collection('messages').document(
-                          current_user_uid).collection(friendUID).add({
-                        'text': msgText,
-                        'time': DateTime.now(),
-                        'to': friendUID,
-                        'from': current_user_uid,
-                      });
-
-                      _firestore.collection('messages')
-                          .document(friendUID)
-                          .collection(current_user_uid)
-                          .add({
-                        'text': msgText,
-                        'time': DateTime.now(),
-                        'from': friendUID,
-                        'to': current_user_uid,
-                      });
-                    },
+                        _firestore.collection('messages')
+                            .document(friendUID)
+                            .collection(current_user_uid)
+                            .add({
+                          'text': msgText,
+                          'time': DateTime.now(),
+                          'from': friendUID,
+                          'to': current_user_uid,
+                        });
+                      },
+                      color: Theme
+                          .of(context)
+                          .splashColor,
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
                   ),
                 ],
               ),
