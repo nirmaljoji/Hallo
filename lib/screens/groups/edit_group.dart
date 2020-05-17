8e5e36107790bbc0819b7e034340b4f8dd822e65
Sharon Joji <sharonjoji99@gmail.com>
2020-05-17 22:54:06 +0530

    members list showing

--- lib/screens/groups/edit_group.dart
+++ lib/screens/groups/edit_group.dart
@@ -133,7 +133,7 @@ class _EditGroupState extends State<EditGroup> {
                             ),
                       );
                     },
-                    child: Text('Edit Admins'),
+                    child: Text('Members'),
                   ),
                 ],
               );
